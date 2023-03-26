e95237cebd554eb51debe4a69cc3941dad11da46
sawatdekrap <sawatdekrap@gmail.com>
2023-03-27 10:13:09 +1100

    Add graphql router

--- server.py
+++ server.py
@@ -1,10 +1,8 @@
 from typing import Union
 
 from fastapi import FastAPI
+from routes.graphql import router as graphql_router
 
 app = FastAPI()
 
-
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
+app.include_router(graphql_router)
